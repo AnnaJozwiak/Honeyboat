e88255e8e6ae0303c94b309b893f8e258c79e54c
AniaJ <114512942+AnnaJozwiak@users.noreply.github.com>
2023-05-06 01:47:30 +0200

    404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Route, Routes, Link, BrowserRouter,HashRouter} from 'react-router-dom';
+import { Route, Routes, Link, BrowserRouter as Router,HashRouter} from 'react-router-dom';
 
 import Main from "./components/Main";
 import AdminPulpit from "./admin/AdminPulpit";
@@ -13,16 +13,16 @@ function App() {
   return (
       <CartProvider>
       <div>
-      <BrowserRouter basename='/Honeyboat'>
+      <Router basename='/Honeyboat'>
       {/*<BrowserRouter basename='/React'>*/}
         <Routes>
           <Route exact path='/' element={<Main />}/>
           <Route path='/admin' element={<AdminPulpit/>}/>
           <Route path='/admin/:id' element={<Update/>} />
           <Route path='/login' element={<Login/>}/>
-          <Route path='/shop' component={<ShopPulpit/>}/>
+          <Route path='/shop' element={<ShopPulpit/>}/>
         </Routes>
-      </BrowserRouter>
+      </Router>
       </div>
       </CartProvider>
   )
